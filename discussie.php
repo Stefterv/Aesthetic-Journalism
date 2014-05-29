5baa6dc6542633c35df5c975d1ae43d1c8302c2f
Stefterv <stef@debarchel.nl>
2014-05-29 21:40:27 +0200

    Added Stylesheets

--- discussie.php
+++ discussie.php
@@ -3,6 +3,7 @@
 <head>
 	<title>De Disccussie</title>
 	<link href="style.css" type="text/css" rel="stylesheet" />
+	<link href="discussie.css" type="text/css" rel="stylesheet" />
 	<meta charset="UTF-8" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="script.js"></script>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Alfredo Jaar’s portal to reality</title>
 	<link href="style.css" type="text/css" rel="stylesheet" />
+	<link href="portal.css" type="text/css" rel="stylesheet" />
 	<meta charset="UTF-8" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="script.js"></script>
