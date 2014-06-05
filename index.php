45dcaf3b91d7e607207700e3a4f9cf18d65cabb1
Stefterv <stef@debarchel.nl>
2014-06-05 18:27:52 +0200

    5 Jun 2014

--- index.php
+++ index.php
@@ -1,39 +1,24 @@
+<?
+	$page = "portal";
+	if(isset($_GET['page'])){
+		$page = $_GET['page'];
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Alfredo Jaar’s portal to reality</title>
 	<link href="style.css" type="text/css" rel="stylesheet" />
-	<link href="portal.css" type="text/css" rel="stylesheet" />
+	<link href="<?=$page?>.css" type="text/css" rel="stylesheet" />
 	<meta charset="UTF-8" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="script.js"></script>
 </head>
 <body>
 <div class="container">
-	<article>
-		<h1>Alfredo Jaar’s portal to reality</h1>
-<h2>Kevin ten Thij</h2>
-
-Een klein <span class="cycle" level-1="afrikaans" level-2="zuidelijk" level-3="het moederlandse">afrikaans</span> meisje, kwetsbaar en dicht bij haar einde maar nog zo jong, een aangrijpend beeld, een flits. Alsof ik zelf gefotografeerd wordt het beeld afgekapt en wordt ik terug in mijn stoel van realiteit gesmeten. Terwijl mijn ogen herstellen begin ik de tekst weer op te pakken waarmee het werk begonnen is. Alfredo Jaar’s Sound of Silence is een werk dat context geeft en de situatie van fotojournalist Kevin Carter uitlegt. Toch bevat het werk maar één beeld. Een beeld dat geen enkele mogelijkheid krijgt zich te vestigen in mijn hoofd, omdat het onderbroken wordt met een oogverblindende flits waarmee het mij in de positie van het meisje op de foto plaatst. Jaar lijkt zich erg bewust van het effect dat een beeld op een mens kan hebben. Hij houdt zich ook in eerdere werken bezig met de realiteit die journalistiek meebrengt en hier lijkt het specifiek te gaan om de realiteit die dit beeld representeert. Maar doet het dat wel? De foto is een vastgelegd moment in onze realiteit, maar het is een fractie van een seconde, uitgesneden uit onze tijdlijn. Het is een fractie van een landschap, uitgesneden uit onze wereld. En een fractie van een conflict, uitgesneden uit onze geschiedenis. Mijn punt is, dit beeld is niet echt meer een documentatie van een realiteit, maar vormt zijn eigen realiteit. Alle ophef over deze ene foto, omschreven in het werk, laat zien dat het publiek alleen dit beeld ziet, en hier een compleet eigen waardeoordeel aan hangt. Het publiek heeft geen idee van de situatie waarin deze foto genomen is en het beeld speelt een rol op zich, het vormt een eigen hoofdstuk in onze geschiedenis omdat het zijn eigen waarde krijgt.<br>
-<br>
-Baudrillard is een van de vele filosofen die zich bezig houden met de connectie tussen de realiteit en fictie, en hoe fictie vaak leidt tot een nieuwe realiteit. Baudrillard stelt 4 fasen. „Dit zouden dan de opeenvolgende fasen van het beeld zijn:<br>
-<ul>
-	<li>het is de weerspiegeling van een fundamentele realiteit</li>
-	<li>het maskeert en perverteert een fundamentele realiteit</li>
-	<li>het maskeert de afwezigheid van een fundamentele realiteit</li>
-	<li>het heeft geen relatie met welke realiteit dan ook: het is zuiver een simulacrum van zichzelf.”</li>
-</ul>
-Het beeld van Kevin Carter bevindt zich in de derde fase van het beeld, eerst is het een weerspiegeling geweest van een fundamentele realiteit. Een fotoreportage van een conflict in zuid Afrika waarbij een lugubere en confronterende realiteit is vastgelegd. Vervolgens is dit beeld als enige realiteit beschouwd in de westerse wereld en gaat het zonder context en uitleg een eigen leven leiden, hiermee maskeert het de rest van de realiteit van het beeld en perverteert het het beeld dat het westen van de situatie heeft. De derde fase bestaat hier uit het idee dat het beeld een accurate weergave is van de situatie en dus geen verdere realiteit behoeft, het geeft de indruk dat dit de realiteit is en maskeert dus de afwezigheid hiervan. <br>
-<br>
-Een belangrijke vergelijking hier in is ook die van Jacques Lacan die een triade stelt van „the imaginary”, „the symbolic” en „the real”. „The imaginary order” is het stadium in de menselijke ontwikkeling waarin hij/zij zich als op zichzelf staand individu gaat zien, los van de ouders. Dit gebeurt door middel van „the sense of self” welke mede gegenereerd door het zien van de weerspiegeling van een persoon zijn eigen verschijning. „When it sees itself in the mirror, however, it does see itself as a whole, but it is only an image of the whole. The other in the mirror is Imaginary. The child will now identify itself with the image. This is called a dual kind of relation.”  „The symbolic” is de fase waarin een mens betekenis gaat geven aan dingen door middel van associatieve symbolisering, een fase waardoor een boom niet langer een boom is maar een boom bestaat uit gezondheid, natuur, zuurstof en alle andere associaties die wij als mens ontwikkelen bij het beeld van een boom. Ten slotte is er „the real”, de daadwerkelijke wereld, het fysieke, het lichaam en de natuur. Al deze elementen zijn volgens Lacan niet uit te drukken in woorden omdat woorden „the symbolic” naar voren halen en onderscheid creëren waar deze er niet is. Maar als men niet in staat is het echte te bevatten, omschrijven en erover te reflecteren, bestaat er in de menselijke bevatting dan wel een echte wereld?<br> 
-Slavoj Zizek gelooft van niet, hij is van mening dat we in onze hedendaagse maatschappij enkel en alleen nog maar in een simulatie leven, dat we geen realiteit meer hebben maar onze realiteit enkel en alleen bestaat uit beelden en symboliseringen. „Are the pessimistic cultural criticists (from Jean Baudrillard to Paul Virilio) justified in their claim that cyberspace ultimately generates a kind of proto-psychotic immersion into an imaginary universe of hallucinations, unconstrained by any symbolic Law or by any impossibility of some Real? If not, how are we to detect in cyberspace the contours of the other two dimensions of the Lacanian triad ISR, the Symbolic and the Real?” Zizek stelt dat we een set parameters moeten stellen om de grenzen van de realiteit duidelijk te markeren alvorens ons over te geven aan een virtuele realiteit van media en symboliek. <br>
-<br>
-Zoals Alfredo Jaar in zijn werk de duidelijke distinctie maakt tussen het beeld van Kevin Carter en de context van de situatie. Hij stelt een grens tussen het visuele en de tekstuele betekenis van deze twee media, het beeld is het middelpunt van het werk, en de aanleiding van het verhaal. De tekst is de toelichting en bevrijding van het beeld door middel van subtiele realiteit en zonder het gebruik van visuele stimulans. Jaar is zich erg bewust van de vorming van onze realiteit onder de invloed van de hedendaagse visuele media, het werk lijkt zich te richten op deze perversie van een realiteit en bevraagt de rol van de media in de vorming van ons wereldbeeld. <br>
-<br>
-Het feit dat Jaar deze 2 elementen splitst is interessant, gezien het feit dat tekst ook een medium is en volgens Lacan duidelijk ook bestaat uit tekens, symbolen en verwijzingen. Jaar lijkt zich hierin dus meer te richten op de aard van de fotografie ten opzichte van de tekst en geeft de tekst daarin een hele andere rol. „‘the medium is the message’ (McLuhan 1994: 7), the catchphrase that implies how new forms of media (rather than content) change the perception of society, and move from a linear way of understanding the world (first oral, then written narratives) to a constant and contradictory flux of signs (which are seen and interpreted).” Als het gaat om een discrepantie tussen een lineaire versus een non-lineaire denkrichting is de keuze van een film op deze wijze interessant. Jaar laat de foto tussen de teksten door verschijnen in de chronologische volgorde van het verhaal en geeft de foto daar mee in zekere zin de rol die een tekst zou hebben in de lineaire manier van analyseren. Een subtiel statement om nogmaals het verschil in impact dat verschillende media op onze realiteit hebben te illustreren. <br>
-<br>
-De ontwikkeling van media zoals het fotografisch en cinematografisch beeld zijn belangrijke ontwikkelingen in de perceptie van de wereld door het menselijk ras, door deze steeds realistischere wijzen van weergeven en dupliceren wordt het moeilijk om nog onderscheid te maken tussen de realiteit en de representatie van realiteit, men kan echter beargumenteren, als een duplicaat een object is met de exacte eigenschappen van het origineel, wat onderscheidt het dan nog, en is het nog van belang om überhaupt onderscheiden te worden? Misschien moeten we ons overgeven, onszelf laten verzwelgen in de wereld van simulaties, media, signalen en reproducties. Tenslotte, als deze wereld een exacte replica is van de wereld waar we ons in begeven, wie merkt dan nog het verschil?
-	</article>
+	<?
+		include($page.".php");
+	?>
 </div>
 </body>
 </html>
