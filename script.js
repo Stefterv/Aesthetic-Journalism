3d604160879f68eae55dad38308d4edb5d748ceb
Stefterv <stef@debarchel.nl>
2014-06-05 18:49:11 +0200

    18:49

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ $(document).ready(function($) {
 		for(var i = 1; i<10; i++){
 			var levelValue = $(this).attr('level-'+i);
 			if(levelValue!=null)maxLevel = i;
-			if(levelValue==$(this).text())level = i;
+			if(levelValue==$(this).html())level = i;
 		}
 		if(level>0 && maxLevel>0){
 			level += 1;
 			if(level>maxLevel)level = 1;
-			$(this).text($(this).attr('level-'+level));
+			$(this).html($(this).attr('level-'+level));
 		}
 	});	
 });
