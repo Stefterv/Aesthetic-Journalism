02e1673a241431d1dd75c6b96ee35e6bb63b1d31
Stefterv <stef@debarchel.nl>
2014-06-05 18:36:00 +0200

    18:35

--- discussie.php
+++ discussie.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>De Disccussie</title>
-	<link href="style.css" type="text/css" rel="stylesheet" />
-	<link href="discussie.css" type="text/css" rel="stylesheet" />
-	<meta charset="UTF-8" />
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="script.js"></script>
-</head>
-<body>
-<div class="container">
-	
-</div>
-</body>
-</html>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
