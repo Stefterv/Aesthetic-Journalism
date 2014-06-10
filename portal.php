f2d0f48658ed2137b7053f13c5faeb68a4090efa
kevintenthij <wouter@mbpvankntenthij.home>
2014-06-10 23:17:23 +0200

    23:17 klaar Kevin yay

--- portal.php
+++ portal.php
@@ -3,11 +3,11 @@
 
 <div id="broodtekst">
 
-Een klein afrikaans meisje, 
+<context relation="test">Een klein afrikaans meisje, 
 
-<span class="cycle" level-1="kwetsbaar" level-2="lichtgeraakt" level-3="teer">kwetsbaar</span> 
+<span class="cycle" level-1="kwetsbaar" level-2="lichtgeraakt" level-3="<a href=/?page=reality class=relation id=kwetsbaar01>teer</a>">kwetsbaar</span> 
 	
-en dicht bij haar einde maar nog zo jong, een aangrijpend beeld, een flits. Het beeld wordt afgekapt en ik wordt terug in de stoel der realiteit geworpen. Terwijl mijn ogen herstellen begin ik de tekst weer op te pakken waarmee het werk begonnen is. Alfredo Jaar’s Sound of Silence is een werk dat 
+en dicht bij haar einde maar nog zo jong, </context>een aangrijpend beeld, een flits. Het beeld wordt afgekapt en ik wordt terug in de stoel der realiteit geworpen. Terwijl mijn ogen herstellen begin ik de tekst weer op te pakken waarmee het werk begonnen is. Alfredo Jaar’s Sound of Silence is een werk dat 
 	
 <span class="cycle" level-1="context" level-2="setting" level-3="perspectief">context</span> 
 	
@@ -15,34 +15,34 @@ geeft en de situatie van fotojournalist Kevin Carter uitlegt. Toch bevat het wer
 	
 <span class="cycle" level-1="positie" level-2="stellingname" level-3="toestand">positie</span> 
 	
-van het meisje op de foto plaatst. Jaar lijkt zich erg bewust van het effect dat een beeld op de mens kan hebben. Hij houdt zich ook in eerdere werken bezig met de realiteit die journalistiek meebrengt en hier lijkt het specifiek te gaan om de realiteit die dit beeld representeert. Maar doet het dat wel? De foto is een vastgelegd moment in onze 
+van het meisje op de foto plaatst. Jaar lijkt zich erg bewust van het effect dat een beeld op de mens kan hebben. Hij houdt zich ook in eerdere werken bezig met de realiteit die journalistiek meebrengt en hier lijkt het specifiek te gaan om de realiteit die dit beeld representeert. <context relation="realiteit01">Maar doet het dat wel? De foto is een vastgelegd moment in onze 
 
 <span class="cycle" level-1="realiteit" level-2="werkelijkheid" level-3="<a href=/?page=discussie class=relation id=realiteit01>wezenlijkheid</a>" >realiteit</span>, 
 	
-maar het is een fractie van een seconde, uitgesneden uit onze tijdlijn. Het is een fractie van een landschap, uitgesneden uit onze wereld. </context> En een fractie van een conflict, uitgesneden uit onze geschiedenis. Mijn punt is: dit beeld is niet echt meer een documentatie van een realiteit, maar vormt zijn eigen realiteit. Alle ophef over deze ene foto, omschreven in het werk, laat zien dat het 
+maar het is een fractie van een seconde, uitgesneden uit onze tijdlijn. Het is een fractie van een landschap, uitgesneden uit onze wereld.  En een fractie van een conflict, uitgesneden uit onze geschiedenis. </context> Mijn punt is: dit beeld is niet echt meer een documentatie van een realiteit, maar vormt zijn eigen realiteit. Alle ophef over deze ene foto, omschreven in het werk, laat zien dat het 
 
 <span class="cycle" level-1="publiek" level-2="gehoor" level-3="gezelschap">publiek</span> 
 	
-alleen dit beeld ziet, en hier een compleet eigen waardeoordeel aan hangt. Het publiek heeft geen idee van de situatie waarin deze foto genomen is en het 
+alleen dit beeld ziet, en hier een compleet eigen waardeoordeel aan hangt. <context relation="beeld01">Het publiek heeft geen idee van de situatie waarin deze foto genomen is en het 
 	
-<span class="cycle" level-1="beeld" level-2="symbool" level-3="overzicht">beeld</span> 
+<span class="cycle" level-1="beeld" level-2="symbool" level-3="<a href=/?page=reality class=relation id=beeld01>overzicht</a>">beeld</span> 
 	
-speelt een rol op zich, het vormt een eigen hoofdstuk in onze geschiedenis omdat het zijn eigen waarde krijgt.<br>
+speelt een rol op zich, het vormt een eigen hoofdstuk in onze geschiedenis omdat het zijn eigen waarde krijgt.<br> </context>
 <br>
 	
-Baudrillard is een van de vele filosofen die zich bezig houden met de connectie tussen de realiteit en 
+Baudrillard is een van de vele filosofen die zich bezig houden met <context relation="fictie01">de connectie tussen de realiteit en 
 	
 <span class="cycle" level-1="fictie" level-2="fantasie" level-3="<a href=/?page=discussie class=relation id=fictie01>verbeelding</a>">fictie</span>
 	
-, en hoe fictie vaak leidt tot een nieuwe realiteit. Baudrillard stelt 4 fasen. <i>„Dit zouden dan de opeenvolgende fasen van het beeld zijn:<br>
+, en hoe fictie vaak leidt tot een nieuwe realiteit. </context>Baudrillard stelt 4 fasen. <i>„Dit zouden dan de opeenvolgende fasen van het beeld zijn:<br>
 	<ul>
 het is de weerspiegeling van een fundamentele realiteit<br>
-het maskeert en 
+<context relation="pervereert01">het maskeert en 
 		
-<span class="cycle" level-1="pervereert" level-2="vervormt">pervereert</span> 
+<span class="cycle" level-1="pervereert" level-2="<a href=/?page=reality class=relation id=pervereert01>vervormt</a>">pervereert</span> 
 		
 	
-een fundamentele realiteit<br>
+een fundamentele realiteit<br></context>
 het maskeert de afwezigheid van een fundamentele realiteit<br>
 het heeft geen relatie met welke realiteit dan ook: het is zuiver een 
 
@@ -59,30 +59,30 @@ realiteit. Een fotoreportage van een conflict in zuid Afrika waarbij een luguber
 	
 <span class="cycle" level-1="leven" level-2="existentie" level-3="teren">leven</span> 
 	
-leiden. Hiermee maskeert het de rest van de realiteit van het beeld en perverteert het het beeld dat het westen van de 
+leiden. <context relation="situatie01">Hiermee maskeert het de rest van de realiteit van het beeld en perverteert het het beeld dat het westen van de 
 	
-<span class="cycle" level-1="situatie" level-2="toestand" level-3="omstandigheid">situatie</span> 
+<span class="cycle" level-1="situatie" level-2="toestand" level-3="<a href=/?page=reality class=relation id=situatie01>omstandigheid</a>">situatie</span> 
 	
 heeft. De derde fase bestaat hier uit het idee dat het beeld een accurate weergave is van de situatie en dus geen verdere realiteit behoeft, het geeft de indruk dat dit de realiteit is en maskeert dus de afwezigheid hiervan. <br>
 <br>
 	
-Een belangrijke vergelijking is ook die van Jacques Lacan die een triade stelt van „the imaginary”, „the symbolic” en „the real”. „The 
+Een belangrijke vergelijking is ook die van Jacques Lacan die een triade stelt van „the imaginary”, „the symbolic” en „the real”. <context relation="self01">„The 
 
 <span class="cycle" level-1="imaginary" level-2="fictional" level-3="hypothetical">imaginary</span> 
 
 order” is het stadium in de menselijke ontwikkeling waarin de mens zich als op zichzelf staand individu gaat zien, los van de ouders. Dit gebeurt door middel van „the sense of 
 	
-<span class="cycle" level-1="self" level-2="autogenous" level-3="subjective">self</span>” 
+<span class="cycle" level-1="self" level-2="autogenous" level-3="<img class=afbeelding src=images/self.png.jpg>"level-4="<a href=/?page=reality class=relation id=self01>subjective</a>">self</span>” 
 	
-welke mede gegenereerd door het zien van de weerspiegeling van een persoon zijn eigen verschijning. <i>„When it sees itself in the mirror, however, it does see itself as a whole, but it is only an image of the whole. The other in the mirror is Imaginary. The child will now identify itself with the image. This is called a dual kind of relation.” <sup>2</sup></i>  „The symbolic” is de fase waarin de mens 
+welke mede gegenereerd door het zien van de weerspiegeling van een persoon zijn eigen verschijning. <i>„When it sees itself in the mirror, however, it does see itself as a whole, but it is only an image of the whole. The other in the mirror is Imaginary. The child will now identify itself with the image. This is called a dual kind of relation.” <sup>2</sup></i></context>  „The symbolic” is de fase waarin de mens 
 	
 <span class="cycle" level-1="betekenis" level-2="inhoud" level-3="belang">betekenis</span> 
 	
-gaat geven aan dingen door middel van associatieve symbolisering; een fase waardoor een boom niet langer een boom is maar een boom bestaat uit gezondheid, natuur, zuurstof en alle andere associaties die wij als mens ontwikkelen bij het beeld van een boom. Ten slotte is er „the real”, de daadwerkelijke wereld, het 
+gaat geven aan dingen door middel van associatieve symbolisering; een fase waardoor een boom niet langer een boom is maar een boom bestaat uit gezondheid, natuur, zuurstof en alle andere associaties die wij als mens ontwikkelen bij het beeld van een boom. <context relation="tastbare01">Ten slotte is er „the real”, de daadwerkelijke wereld, het 
 
 <span class="cycle" level-1="fysieke" level-2="lijfelijk" level-3="<a href=/?page=discussie class=relation id=tastbare01>tastbare</a>">fysieke</span>
 	
-, het lichaam en de natuur. Al deze elementen zijn volgens Lacan niet uit te drukken in woorden omdat woorden „the symbolic” naar voren halen en onderscheid creëren waar deze er niet is. Maar als men niet in staat is het echte te 
+, het lichaam en de natuur. Al deze elementen zijn volgens Lacan niet uit te drukken in woorden omdat woorden „the symbolic” naar voren halen en onderscheid creëren waar deze er niet is. </context> Maar als men niet in staat is het echte te 
 	
 <span class="cycle" level-1="bevatten" level-2="behelzen" level-3="doorzien">bevatten</span>
 	
@@ -99,21 +99,21 @@ Hij zegt hierover: <i>„Are the pessimistic cultural criticists (from Jean Baud
 	
 <span class="cycle" level-1="generates" level-2="develop" level-3="set up">generates</span> 
 	
-a kind of proto-psychotic immersion into an imaginary universe of hallucinations, unconstrained by any symbolic Law or by any impossibility of some Real? If not, how are we to detect in cyberspace the contours of the other two dimensions of the Lacanian triad ISR, the Symbolic and the Real?” <sup>3</sup></i> Zizek stelt dat we een set parameters moeten stellen om de grenzen van de realiteit duidelijk te markeren alvorens ons over te geven aan een virtuele realiteit van media en 
+a kind of proto-psychotic immersion into an imaginary universe of hallucinations, unconstrained by any symbolic Law or by any impossibility of some Real? If not, how are we to detect in cyberspace the contours of the other two dimensions of the Lacanian triad ISR, the Symbolic and the Real?” <sup>3</sup></i> <context relation="symboliek01">Zizek stelt dat we een set parameters moeten stellen om de grenzen van de realiteit duidelijk te markeren alvorens ons over te geven aan een virtuele realiteit van media en 
 	
-<span class="cycle" level-1="symboliek" level-2="vertegenwoordiging">symboliek</span>
+<span class="cycle" level-1="symboliek" level-2="<a href=/?page=reality class=relation id=symboliek01>vertegenwoordiging</a>">symboliek</span>
 	
 . <br>
-<br>
-Zoals Alfredo Jaar in zijn werk de duidelijke distinctie maakt tussen het beeld van Kevin Carter en de context van de situatie stelt hij ook een grens tussen het 
+<br> </context>
+<context relation="visuele01">Zoals Alfredo Jaar in zijn werk de duidelijke distinctie maakt tussen het beeld van Kevin Carter en de context van de situatie stelt hij ook een grens tussen het 
 
-<span class="cycle" level-1="visuele" level-2="optische" level-3="zienbaar">visuele</span> 
+<span class="cycle" level-1="visuele" level-2="optische" level-3="<a href=/?page=reality class=relation id=visuele01>zienbaar</a>">visuele</span> 
 	
 en de tekstuele betekenis van deze twee media, het beeld is het middelpunt van het werk, en de aanleiding van het verhaal. De tekst is de toelichting en bevrijding van het beeld door middel van 
 
 <span class="cycle" level-1="subtiele" level-2="kleine" level-3="onopvallende">subtiele</span> 
 	
-realiteit en zonder het gebruik van visuele stimulans. Jaar is zich erg bewust van de vorming van onze realiteit onder de invloed van de hedendaagse visuele media, het werk lijkt zich te richten op deze 
+realiteit en zonder het gebruik van visuele stimulans.</context> Jaar is zich erg bewust van de vorming van onze realiteit onder de invloed van de hedendaagse visuele media, het werk lijkt zich te richten op deze 
 	
 <span class="cycle" level-1="perversie" level-2="corruptie" level-3="immoraliteit">perversie</span> 
 	
@@ -121,41 +121,41 @@ van een realiteit en bevraagt de rol van de media in de vorming van ons wereldbe
 <br>
 Het feit dat Jaar deze 2 elementen splitst is interessant, gezien het feit dat tekst ook een medium is en volgens Lacan duidelijk ook bestaat uit tekens, symbolen en 
 
-<span class="cycle" level-1="verwijzingen" level-2="referenties" level-3="betrekkingen">verwijzingen</span>. 
+<span class="cycle" level-1="verwijzingen" level-2="referenties" level-3="betrekkingen" level-4="<img class=afbeelding src=images/verwijzingen.png>">verwijzingen</span>. 
 	
-Jaar lijkt zich hierin dus meer te richten op de aard van de fotografie ten opzichte van de tekst en geeft de tekst daarin een hele andere rol. <i>„‘the medium is the message’ (McLuhan 1994: 7), the catchphrase that implies how new forms of media (rather than content) change the 
+Jaar lijkt zich hierin dus meer te richten op de aard van de fotografie ten opzichte van de tekst en geeft de tekst daarin een hele andere rol. <i><context relation="perception01">„‘the medium is the message’ (McLuhan 1994: 7), the catchphrase that implies how new forms of media (rather than content) change the 
 	
 <span class="cycle" level-1="perception" level-2="impression" level-3="<a href=/?page=discussie class=relation id=perception01>sense</a>">perception</span> 
 	
 of society, and move from a linear way of understanding the world (first oral, then written narratives) to a constant and contradictory flux of signs (which are seen and 
 	
-<span class="cycle" level-1="interpreted" level-2="explained" level-3="understood">interpreted</span>
+<span class="cycle" level-1="interpreted" level-2="explained" level-3="<a href=/?page=reality class=relation id=interpreted01>understood</a>">interpreted</span>
 	
-).” <sup>4</sup></i> Als het gaat om een discrepantie tussen een lineaire en een non-lineaire denkrichting is de keuze van een film op deze wijze interessant. Jaar laat de foto tussen de teksten door verschijnen in de 
+).”</context> <sup>4</sup></i> Als het gaat om een discrepantie tussen een lineaire en een non-lineaire denkrichting is de keuze van een film op deze wijze interessant. Jaar laat de foto tussen de teksten door verschijnen in de 
 	
-<span class="cycle" level-1="chronologische" level-2="sequentiele">chronologische</span> 
+<span class="cycle" level-1="chronologische" level-2="sequentiele" level-3="<img class=afbeelding src=images/tijd.jpg>">chronologische</span> 
 	
 	volgorde van het verhaal en geeft de foto daarmee in zekere zin de rol die een tekst zou hebben in de lineaire manier van <span class="cycle" level-1="analyseren" level-2="evalueren" level-3="inspecteren">analyseren</span>
 	
 . Een subtiel statement om nogmaals het verschil in impact dat verschillende media op onze realiteit hebben te illustreren. <br>
 <br>
-De ontwikkeling van media zoals het fotografische en cinematografische beeld zijn belangrijke 
+<context relation="replica01"><context relation="origineel01"><context relation="ontwikkelingen01"><context relation="mens01">De ontwikkeling van media zoals het fotografische en cinematografische beeld zijn belangrijke 
 
-<span class="cycle" level-1="ontwikkelingen" level-2="progressie" level-3="veranderingen" >ontwikkelingen</span>
+<span class="cycle" level-1="ontwikkelingen" level-2="progressie" level-3="<a href=/?page=reality class=relation id=ontwikkelingen01>veranderingen</a>" >ontwikkelingen</span>
 	
 in de perceptie van de wereld door de 
 	
 <span class="cycle" level-1="mens" level-2="<a href=/?page=discussie class=relation id=mens01>homosapien</a>">mens</span>
 
-. Door deze steeds realistischere wijzen van weergeven en dupliceren wordt het moeilijk om nog onderscheid te maken tussen de realiteit en de representatie van realiteit. Men kan echter beargumenteren: als een duplicaat een object is met de exacte eigenschappen van het 
+. Door deze steeds realistischere wijzen van weergeven en dupliceren wordt het moeilijk om nog onderscheid te maken tussen de realiteit en de representatie van realiteit.</context></context> Men kan echter beargumenteren: als een duplicaat een object is met de exacte eigenschappen van het 
 
-<span class="cycle" level-1="origineel" level-2="oorspronkelijke" level-3="authentieke">origineel</span>
+<span class="cycle" level-1="origineel" level-2="oorspronkelijke" level-3="<a href=/?page=reality class=relation id=origineel01>authentieke</a>">origineel</span>
 	
 , wat onderscheidt het dan nog, en is het nog van belang om überhaupt onderscheiden te worden? Misschien moeten we ons overgeven, onszelf laten verzwelgen in de wereld van simulaties, media, signalen en reproducties. Tenslotte, als deze wereld een exacte 
 	
-<span class="cycle" level-1="replica" level-2="verdubbeling" level-3="kopie">replica</span> 
+<span class="cycle" level-1="replica" level-2="verdubbeling" level-3="<img class=afbeelding src=images/replica.jpg>" level-4="<a href=/?page=reality class=relation id=replica01>kopie</a>">replica</span> 
 	
-is van de wereld waar we ons in begeven, wie merkt dan nog het verschil?
+is van de wereld waar we ons in begeven, wie merkt dan nog het verschil?</context></context>
 	
 	<br><br>
 	
